7634fe47592d21b65835d0ebbdd3eb0d4eadb95b
Fernando Carvalho <84986212+Fer96carvalho@users.noreply.github.com>
2025-02-26 19:43:17 -0300

    Update footer.tsx

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <div className="w-full bg-secondary py-6 px-5">
+        <div className="w-full bg-secondary py-6 px-5 mt-1">
             <p className="text-gray-400 text-xs font-bold opacity-75">® 2025 Copyright X-DataLink</p>
         </div>
      );
