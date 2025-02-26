04cfc00b0e9aef63264c47281712ea850ebee7ff
Fernando Carvalho <84986212+Fer96carvalho@users.noreply.github.com>
2025-02-26 19:42:37 -0300

    Update side-menu.tsx

--- app/_components/side-menu.tsx
+++ app/_components/side-menu.tsx
@@ -2,8 +2,6 @@
 
 import { SheetHeader, SheetTitle } from "./ui/sheet";
 import {
-  CalendarIcon,
-  HomeIcon,
   LogInIcon,
   LogOutIcon,
   UserIcon,
@@ -11,43 +9,21 @@ import {
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./ui/button";
-import Link from "next/link";
-import { useEffect, useState } from "react";
 
 const SideMenu = () => {
+  const api = process.env.API_URL;
   const { data } = useSession();
 
-  async function createUser(data: any) {
-      if (data) {
-        try {
-        const user = await fetch(`http://localhost:4000/login`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${data.user.idToken ?? ""}`,
-            },
-            body: JSON.stringify({
-              data
-            })
-          }).then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            console.log("Response", response);
-          });
-        } catch (error) {
-          console.error("Erro ao conectar com a API", error);
-        }
-      }
-    }
+  const handleLoginClick = async () => {
+    const result = await signIn("google", { redirect: false, prompt: 'select_account'});
+  };
+  
+  const handleLogoutClick = async() => {
+    const data = await fetch(`${api}/logout`, {
+    })
+    signOut();
 
-    createUser(data);
-
-  const handleLoginClick = async () => signIn("google", { callbackUrl: "/" });
-
-  console.log("Data", data);
-
-  const handleLogoutClick = () => signOut();
+  }
 
   return (
     <>
@@ -58,13 +34,16 @@ const SideMenu = () => {
         <div className="flex justify-between px-5 py-6 items-center">
           <div className="flex items-center gap-3 ">
             <Avatar>
-              <AvatarImage src={data.user?.image ?? ""} />
+              <AvatarImage src={`${data.user.image}`} />
             </Avatar>
             <h2 className="font-bold">{data.user.name}</h2>
           </div>
+          <div className="w-fit">
+
           <Button variant="secondary" size="icon">
             <LogOutIcon onClick={handleLogoutClick} />
           </Button>
+          </div>
         </div>
       ) : (
         <div className="px-5 py-6 flex flex-col gap-3">
@@ -72,32 +51,26 @@ const SideMenu = () => {
             <UserIcon size={32} />
             <h2 className="font-bold">Olá, faça seu login!</h2>
           </div>
+          <div className="w-fit">
           <Button
             variant="secondary"
             className="w-full justify-start"
             onClick={handleLoginClick}
-          >
+            >
             <LogInIcon className="mr-2" size={18} />
             Fazer Login
           </Button>
+            </div>
         </div>
       )}
-      <div className="flex flex-col gap-3 px-5">
+      {/* <div className="flex flex-col gap-2 px-5 w-fit">
         <Button variant="outline" className="justify-start" asChild>
           <Link href="/">
             <HomeIcon size={18} className="mr-2" />
             Início
           </Link>
         </Button>
-        {/* {data?.user && (
-          <Button variant="outline" className="justify-start" asChild>
-            <Link href="/bookings">
-              <CalendarIcon size={18} className="mr-2" />
-              Agendamentos
-            </Link>
-          </Button>
-        )} */}
-      </div>
+      </div> */}
     </>
   );
 };
